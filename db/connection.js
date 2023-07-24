b6e4255e5758852ea3f10f75941ebb076e17158a
Zero_X <rvkov536@gmail.com>
2023-07-24 09:56:12 -0400

    updated connection and server js

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,17 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const isProduction = process.env.PORT;
+let sequelize;
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql'
-});
+if (isProduction) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: 'mysql'
+  });
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  });
+}
+
 
 module.exports = sequelize;
